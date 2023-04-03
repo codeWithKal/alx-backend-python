9690c0237456892a0a439e700182013e5b07ede9
kalu_coder <kaleabshiferaw51@gmail.com>
2023-04-03 21:17:50 +0300

    a concurrent async function runner

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+A module for concurrent coroutines
+'''
+import asyncio as ao
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax.py').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    a method returning list of second delays
+    '''
+    waiting_seconds = await ao.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n))))
+    return waiting_seconds

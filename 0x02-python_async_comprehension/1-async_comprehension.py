fc2df09e5701049b0cbee419571c962b392cab3c
kalu_coder <kaleabshiferaw51@gmail.com>
2023-04-05 02:46:33 +0300

    importlib module used

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 a module for returning list of numbers generated asynchronously
 '''
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
+import importlib
+async_generator = importlib.import_module('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List(float):
     '''
-    a method returning a list of random float numbers
+    a method returning 10 list of random float numbers
     '''
     return [i async for i in async_generator()]

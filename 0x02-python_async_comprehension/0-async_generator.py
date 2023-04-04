28c8ff6bf1f866557c5660070580128dfd875b9f
kalu_coder <kaleabshiferaw51@gmail.com>
2023-04-04 17:55:47 +0300

    initial commit to first task

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Task 0
+'''
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''
+    A method that performs an async generator
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(1, 10)
